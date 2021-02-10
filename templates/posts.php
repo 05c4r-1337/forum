d0512956cca8a94319d6c3be5ab1cc12a12e9bfe
05c4r-1337 <oscar.carl.johansson@gmail.com>
2021-02-10 15:33:35 +0100

    d

---
+++ templates/posts.php
@@ -0,0 +1,24 @@
+<div class="post w-50 m-3 ml-5 mt-5">
+  <h5 class="ml-4 mt-2 d-inline-block">test</h5>
+  <p class="d-block ml-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</div>
+
+<div class="post w-50 m-3 ml-5">
+  <h5 class="ml-4 mt-2 d-inline-block">test</h5>
+  <p class="d-block ml-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</div>
+
+<div class="post w-50 m-3 ml-5">
+  <h5 class="ml-4 mt-2 d-inline-block">test</h5>
+  <img src="../img/china.jpg" alt="" class="d-block ml-4 m-3">
+</div>
+
+<div class="post w-50 m-3 ml-5">
+  <h5 class="ml-4 mt-2 d-inline-block">test</h5>
+  <p class="d-block ml-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</div>
+
+<div class="post w-50 m-3 ml-5">
+  <h5 class="ml-4 mt-2 d-inline-block">test</h5>
+  <p class="d-block ml-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</div>
